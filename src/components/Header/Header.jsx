355e1caeb1f5fef6c70c1f64f016b75f4e9b0e93
KandaPilouf <samikhan199758@gmail.com>
2026-01-11 13:01:29 +0100

    -Moved Sidebar.jsx code into Header.jsx (openin/closing sidebar issue corrected) -Header / Sidebar completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import styles from "./Header.module.css";
 import Button from "../Button/Button.jsx";
-import Sidebar from "../Sidebar/Sidebar.jsx";
 
 function Header() {
-  const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(true);
 
   return (
     <>
+      {/* Header */}
+
       <header className={styles.header}>
         <nav>
           <ul>
@@ -63,7 +64,46 @@ function Header() {
           </ul>
         </nav>
       </header>
-      {showNav && <Sidebar />}
+
+      {/* Sidebar */}
+
+      <div
+        className={
+          showNav ? styles.sidebar : `${styles.sidebar} ${styles.active}`
+        }
+      >
+        <ul>
+          <li>
+            <svg
+              onClick={() => setShowNav(!showNav)}
+              height="40px"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#000000"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path d="M23 13H1v-2h22z"></path>
+                <path fill="none" d="M0 0h24v24H0z"></path>
+              </g>
+            </svg>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <Button />
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
